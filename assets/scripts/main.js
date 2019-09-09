420af9b96c5a98eb9f446692964289e3205108fb
matteuc <ymc006@ucsd.edu>
2019-09-09 00:00:46 -0700

    added lil liri and welearn

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,4 +2,9 @@
     AOS.init();
     $('.tooltipped').tooltip();
 
+    // Disable empty links from refreshing the page
+    $('a[href="#"]').click(function(e) {
+      e.preventDefault();
+    });
+
   });
